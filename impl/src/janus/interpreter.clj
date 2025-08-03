db1a1ee859a9aaf472b02a83017891c494da8b40
Thomas Getgood <thomas@macroexpanse.com>
2025-08-03 17:19:42 -0400

    Detect cycles in the interpreter.

--- impl/src/janus/interpreter.clj
+++ impl/src/janus/interpreter.clj
@@ -162,14 +162,19 @@
       (trace! "result:" rule "\n" sexp "\n->\n" v)
       (debug/tag v rule sexp))))
 
+(def ^:dynamic *seen* #{})
+(defonce t (atom nil))
+
 (defn walk*
   ([sexp]
    (trace! "\n  pass:\n")
    (let [next (walk1 sexp)]
      (cond
-       (= sexp next)   sexp
-       (nil? next)     (assert false "inconceivable!")
-       true            (recur next))))
+       (= sexp next)         sexp
+       (contains? *seen* next) (do (reset! t [next *seen*])
+                                   (throw (RuntimeException. "looping!!!" )))
+       (nil? next)           (assert false "inconceivable!")
+       true                  (binding [*seen* (conj *seen* next)] (walk next)))))
   ([env sexp]
    (walk* (env/pin sexp env))))
 
