f2fde047119f11c1d9a427d2a7e1912758401248
Thomas Getgood <thomas@macroexpanse.com>
2025-11-11 18:24:09 -0500

    env take ???

--- impl/src/xprl/env.clj
+++ impl/src/xprl/env.clj
@@ -30,6 +30,10 @@
   (assert (ast/unresolved? sym) sym)
   (get env sym))
 
+(defn ns-resolved? [sym env]
+  (and (ast/resolved? sym)
+       (not (contains? (:unresolve env) (strip (ast/sym sym))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; Lexical env in AST
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -78,20 +82,16 @@
   [form env]
   (attach form (dyn->lex env)))
 
-(defonce tt (atom nil))
-
 (defn bindμ [{:keys [name params body] :as μ} args]
   (trace! "binding μ:" (merge {params args} (when name {name μ})))
-  (reset! tt {:in {params args}
-              :out (attach body {:bindings (merge {params args} (when name {name μ}))})})
   (attach body {:bindings (merge {params args} (when name {name μ}))}))
 
 (defn block [form & syms]
   (trace! "blocking:" syms)
-  (attach form {:blocks (set syms)}))
+  (attach form {:blocks (set (map strip syms))}))
 
 (defn capture [args]
-  (let [names (mapv ast/sym (butlast args))]
+  (let [names (mapv strip (map ast/sym (butlast args)))]
     (when (every? ast/unresolved? names)
       (conj names (last args)))))
 
@@ -99,23 +99,25 @@
 ;;;;; Dynamic env During Interpretation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def empty-env {:bindings {} :ctx {}})
+(def empty-env ::root)
 
 (defn popbind [m k]
-  (update m k #(if (empty? %) % (pop %))))
+  (update m (strip k) #(if (empty? %) % (pop %))))
 
 (defn pushbind [m [k v]]
-  (update m k conj v))
+  (trace! "pushing binding" k "=" v)
+  (update m (strip k) conj v))
 
 (defn walk-μ [env {:keys [name params]}]
   (-> env
       (assoc :μ? true)
       ;; Here's a tricky one. We don't want any bindings for `name` or `params`
       ;; to leak through when walking the body of a μ: they must remain
-      ;; unresolved. So while walking we remove those bindings completely and
-      ;; rely on them being reinserted on any future traversal where args are
-      ;; applied to the μ.
-      (update :bindings dissoc name params)))
+      ;; unbound. So while walking we remove those bindings completely and rely
+      ;; on them being reinserted on any future traversal where args are applied
+      ;; to the μ.
+      (update :bindings dissoc name params)
+      (update :unresolve (set (remove nil? [name params])))))
 
 (defn walk-channels [env {:keys [chs]}]
   (update env :ctx merge chs))
@@ -127,17 +129,72 @@
                              (reduce pushbind bindings (:bindings local))))
     env))
 
-(defn bound? [{:keys [bindings]} sym]
-  (let [s (ast/sym sym)]
+(defn bound? [sym env]
+  (let [{:keys [bindings]} (incorporate sym env)
+        s                  (strip (ast/sym sym))]
     (when (contains? bindings s)
       (not (empty? (get bindings s))))))
 
-(defn resolve [{:keys [bindings]} sym]
-  (let [s   (ast/sym sym)
-        res (peek (get bindings s))]
+(defn resolve [sym env]
+  (let [{:keys [bindings]} (incorporate sym env)
+        s                  (strip (ast/sym sym))
+        res                (peek (get bindings s))]
     (block res s)))
 
-(defmacro propagate [form env body]
-  {:style/indent 2}
-  `(let [~env (incorporate ~form ~env)]
-     (attach-dyn ~body ~env)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Frames Rewrite
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def empty-env ::root)
+(def μ-env {::μ? true})
+
+(defn μ-ctx? [env]
+  (cond
+    (= ::root env) false
+    (::μ? env)     true
+    true           (recur (::previous env))))
+
+(defn push [env frame]
+  (assoc frame ::previous env))
+
+;; Not reverse! something much weirder.
+(defn invert [s]
+  (if (= (::previous s) ::root)
+    s
+    (recur (assoc (::previous s) ::next s))))
+
+(defn merge-stacks
+  ;; AKA despaghettify
+  "Given two stacks, find their common root and create a new stack of the form
+  root<-unique part of `outer`<-unique part of `inner`."
+  [inner outer]
+  (if (= inner outer) ; this case would lead to infinite looping below
+    inner
+    (loop [i (invert inner)
+           o (invert outer)]
+      (if (= (::previous (::next i)) (::previous (::next o)))
+        (recur (::next i) (::next o))
+        (loop [root (::previous i)
+               o    o]
+          (if (contains? o ::next)
+            (recur (push root (dissoc o ::next)) (::next o))
+            (loop [root (push root o)
+                   i    i]
+              (if (contains? i ::next)
+                (recur (push root (dissoc i ::next)) (::next i))
+                (push root i)))))))))
+
+(defn attach [env form]
+  (cond
+    (vector? form) (into [] (map (partial attach env)) form)
+    (map? form)    (assoc form ::env env)
+    true           form))
+
+(defn resolve [env {:keys [sym] :as im}]
+  (let [s (strip (ast/sym sym))]
+    (loop [{:keys [bindings] :as env} env]
+      (cond
+        (= ::root env)         im
+        (contains? bindings s) (let [next (get bindings s)] ; `next` might be `false`!
+                                 (attach next (merge-stacks (::env next) (::previous env))))
+        true                   (recur (::previous env))))))
