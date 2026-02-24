e6d6fecd686658f6236bf612a0f9d1e798c89912
Thomas Getgood <thomas@macroexpanse.com>
2026-02-24 12:54:43 -0500

    Rewrote and simplified the interpreter yet again.

--- impl/src/xprl/interpreter.clj
+++ impl/src/xprl/interpreter.clj
@@ -1,92 +1,64 @@
-(ns xprl.interpreter
+(ns xprl.c2
   (:refer-clojure :exclude [resolve eval apply])
   (:require
    [xprl.ast :as ast]
-   [xprl.debug :as debug :refer [deftracefn]]
-   [xprl.env :as env]
-   [xprl.system :as sys]))
+   [xprl.debug :as debug :refer [deftracefn]]))
 
 (declare walk)
 
-;;;;; Apply
+(defn call [env f t]
+  ((:fn f) env f t))
 
-(defn apply-error [app]
-  (throw (RuntimeException. (str (:head app) " is not applicable!\n" app))))
-
-(deftracefn apply [{:keys [head tail] :as form} opts]
-  (cond
-    (ast/external? head) ((:fn head) form opts) ; Punt to external interpreter.
-    (ast/μ? head)        (let [tail (if (ast/incomplete? tail) (walk tail opts) tail)]
-                           (if (and (= head tail) (not (:rec? opts)))
-                             (assoc form :tail tail)
-                             (env/bindargs head tail)))
-    true                 (apply-error form)))
-
-;;;;; Eval
-
-(defn resolve [{sym :form :as im} opts]
-  (cond
-    (ast/symbol? sym) im
-    (ast/ref? sym)    (:binding sym)
-    true              (assert false "unreachable!")))
-
-(deftracefn eval [{form :form :as im} opts]
-  (cond
-    (ast/symbolic? form) (resolve im opts)
-    ;; (I (P x y)) => (A (I x) y)
-    (ast/pair? form)     (ast/application (ast/immediate (:head form)) (:tail form))
-    ;; (I (L x y ...)) => (L (I x) (I y) ...)
-    (vector? form)       (into [] (map ast/immediate) form)
-    ;; (I {x y ...}) => {(I x) (I y) ...}
-    ;; FIXME: maps are a pain in the ass because records are maps...
-    (ast/map? form)      (into {} (map #(mapv ast/immediate %)) form)
-    ;; (I V) => V. values are fixed points of eval.
-    true                 form))
-
-;;;;; Walk (previously `reduce`)
-
-(deftracefn walk [form opts]
-  (sys/with-return-ctx opts
+(defn apply [env head tail]
+  (let [env (ast/merge-local-env env head)]
     (cond
-      (ast/immediate? form)   (if (ast/incomplete? (:form form))
-                                (let [form (update form :form walk opts)]
-                                  (if (ast/incomplete? (:form form))
-                                    form
-                                    (eval form opts)))
-                                (eval form opts))
-      (ast/application? form) (if (ast/incomplete? (:head form))
-                                ;; FIXME: This `rec?` business is a kludge
-                                ;; because I don't have a general stopping
-                                ;; condition by which to detect when I'm in an
-                                ;; infinite regress. It's entirely possible that
-                                ;; I'm tripping on the halting problem, which is
-                                ;; just peachy, but let's verify that before we
-                                ;; panic...
-                                (let [rec? (not (ast/incomplete? (:tail form)))
-                                      form (update form :head walk (assoc opts :rec? rec?))]
-                                  (if (ast/incomplete? (:head form))
-                                    (update form :tail walk opts)
-                                    (apply form opts)))
-                                (apply form opts))
+      (ast/μ? head)          (walk (bind env (:id head) (walk env tail)) (:body head))
+      (ast/macro? head)      (call env head tail)
+      (ast/external? head)   (call env head (walk env tail))
+      (ast/incomplete? head) (ast/application env head (walk env tail))
+      true                   (throw (RuntimeException. (str head " is not applicable!"))))))
 
-      (ast/emission? form) (sys/try-emissions! (update form :kvs walk opts) opts)
-      (ast/ctx? form)      (sys/walk-ctx form
-                             (update form :form walk (assoc opts :return-ctx? true)))
+(defn bind [env id val]
+  (assoc-in env [:bindings id] val))
 
-      ;; Pairs are data! walking them can lead to evaluation out of context!
-      ;; But then μs are also data, so why do we walk them? just an optimisation?
-      (ast/μ? form)    (update form :body walk (assoc opts :freeze? true))
-      (ast/list? form) (into [] (map #(walk % opts)) form)
-      (ast/map? form)  (into {} (map (fn [e] (mapv (fn [x] (walk x opts)) e))) form)
-      (= :error form)  (throw (RuntimeException. "fatal error"))
-      true             form)))
+(defn resolve [env form]
+  (let [env (ast/merge-local-env env form)]
+    (cond
+      (ast/input? form)  (if (contains? (:bindings env) (:id form))
+                           (get-in env [:bindings (:id form)])
+                           (ast/immediate form))
+      (ast/ref? form)    (:binding form)
+      (ast/symbol? form) (throw (RuntimeException. (str "unbound symbol: " form)))
+      true               (assert false "unreachable!!"))))
 
-;; Rewalk until fixed point. Is this really the best I can do?
+(defn eval [env form]
+  (let [env (ast/merge-local-env env form)]
+    (cond
+      (ast/pair? form)       (apply env (walk env (ast/immediate (:head form))) (:tail form))
+      (ast/symbolic? form)   (resolve env form)
+      (ast/coll? form)       (into (empty form) (map (partial eval env)) form)
+      (ast/incomplete? form) (ast/immediate form)
+      true                   form)))
 
-(defn interpret [form]
-  (loop [form form]
-    (debug/trace! "start")
-    (let [next (walk form {})]
-      (if (= next form)
-        form
-        (recur next)))))
+(defn walk [env form]
+  (let [env (ast/merge-local-env env form)]
+    (cond
+      (ast/immediate? form)   (eval env (walk env (:form form)))
+      (ast/application? form) (apply env (walk env (:head form)) (:tail form))
+      (ast/symbolic? form)    (let [sym (ast/symbol form)]
+                                (if (contains? (:captured env) sym)
+                                  (ast/input env sym (get-in env [:captured sym]))
+                                  (ast/with-env form env)))
+      (ast/μ? form)           (update form :body #(walk env %))
+      (ast/coll? form)        (into (empty form) (map (partial walk env)) form)
+      true                    form))) ; REVIEW: Do I need ast/merge-env?
+
+
+;; FIXME: These are the two cases I've dropped from the previous interpreter impl:
+;;
+;; (ast/emission? form) (sys/try-emissions! (update form :kvs walk opts) opts)
+;; (ast/ctx? form)      (sys/walk-ctx form
+;;                        (update form :form walk (assoc opts :return-ctx? true)))
+;;
+;; They're not going to work without rewriting, but I'll keep them around for
+;; the reference until I get to it.
