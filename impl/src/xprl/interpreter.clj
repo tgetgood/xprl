edc5bd79e1af5d5e62d261ae515dcbadb75d9ae7
Thomas Getgood <thomas@macroexpanse.com>
2026-06-05 10:15:06 -0400

    Added running commentary.
    I've only changed interpreter.clj, but the executor and builtins need to be
    reworked so that they all play together.
    
    I think I also need to change the way μs operate as well, possibly fortunately,
    definitely annoyingly.

--- impl/src/xprl/interpreter.clj
+++ impl/src/xprl/interpreter.clj
@@ -6,6 +6,23 @@
 
 (declare walk)
 
+;; REVIEW: This needs to be like a builtin primitive (Extern), but it needs to
+;; encapsulate the lexical env so that when the executor runs it with the
+;; execution env, the exec env gets ignored and the lexical env (which is
+;; derived from the exec env) is used instead. So it's not a real primitive.
+;;
+;; This raises a bigger point about when it is and isn't appropriate to insert
+;; the cable. It turns out to only be appropriate at the "top level" which
+;; concept needs to be properly defined soonish.
+;;
+;; I *think* that we really only ought to connect the cable when "seeding" the
+;; executor. Everything called from that point down ought to carry its own
+;; extension of the "root cable" which should be used instead.
+(defn ptask [cb]
+  (fn [arg]
+    (fn [_]
+      (cb arg))))
+
 (defn with-return [env rf f]
   (let [env (assoc env (ast/xkey :return) (ptask rf))]
     (f env)))
@@ -14,9 +31,19 @@
   (ast/emission env [[(ast/xkey :return) v]]))
 
 (defn call! [env f args]
-  (ast/emission env [[:xprl.executor/new-task! [f args]]]))
+  (with-return env f (return! args)))
 
 (defn walk-emission [env em]
+  ;; FIXME: This should be handled by builtins/emit! `walk` shouldn't need to
+  ;; worry about Emission records.
+  ;;
+  ;; REVIEW: What about emissions buried in μs? Will the capture/bind walkers
+  ;; handle this properly? What about something like `(μ x (emit ~(f x) 42))` ?
+  ;;
+  ;; That will break without Emission walking until we bring back reactive inputs
+  ;;
+  ;; There are a lot of moving parts here and I don't want to mash the gears
+  ;; ...again...
   (update em :msgs (fn [xs] (into [] (map (fn [[k v]] [(walk env k) v])) xs))))
 
 (deftracefn apply [env head tail]
