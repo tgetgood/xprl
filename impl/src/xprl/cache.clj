34f0b207873515952a84481519d942a6d0951684
Thomas Getgood <thomas@macroexpanse.com>
2026-06-16 12:43:22 -0400

    Memoisation is no longer necessary.
    And the [a b . c] destructuring test fails with memoisation, which indicates
    something mre deeply wrong.

--- impl/src/xprl/cache.clj
+++ impl/src/xprl/cache.clj
@@ -4,7 +4,7 @@
 ;; FIXME: This is NOT THREAD SAFE.
 ;; I ~think~ that the practical reprecussions in this instance amount to wasted
 ;; effort, but really I need to put a queue in front of this or something.
-(defn weak-memo [f]
+#_(defn weak-memo [f]
   (let [cache (WeakHashMap.)]
     (fn [& args]
       (if-let [v (.get cache args)]
@@ -12,3 +12,7 @@
         (let [v (apply f args)]
           (.put cache args v)
           v)))))
+
+;; REVIEW: I only need caching when I do dumb things, so I'm better off using it
+;; as a red flag, yes?
+(def weak-memo identity)
